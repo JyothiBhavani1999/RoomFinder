a0fb36b21cbdb039e79943ceadb6debc0d3b7a7e
JyothiBhavani1999 <bhavanijyothi182@gmail.com>
2025-11-08 16:04:42 -0600

    add-room

--- RoomFinder/src/main/java/com/jyothi/RoomFinder/Controller/RoomController.java
+++ RoomFinder/src/main/java/com/jyothi/RoomFinder/Controller/RoomController.java
@@ -1,4 +1,38 @@
 package com.jyothi.RoomFinder.Controller;
 
+import com.jyothi.RoomFinder.Model.Room;
+import com.jyothi.RoomFinder.Repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
 public class RoomController {
+    @Autowired
+    RoomRepository roomRepository;
+
+    @GetMapping
+    public String hello() {
+        return "Hello, Room Finder!";
+    }
+
+    @GetMapping("/rooms")
+    public List<Room> getRooms() {
+        return roomRepository.findAll();
+    }
+
+    @GetMapping("/add-room")
+    public String showForm(){
+        return "add-room";
+    }
+
+    @PostMapping("/add-room")
+    public String addRoom(@ModelAttribute Room room) {
+         roomRepository.save(room);
+         return "success";
+    }
+
 }
--- RoomFinder/src/main/java/com/jyothi/RoomFinder/Model/Room.java
+++ RoomFinder/src/main/java/com/jyothi/RoomFinder/Model/Room.java
@@ -2,13 +2,51 @@ package com.jyothi.RoomFinder.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Room {
 
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Id
     private Long id;
     private String city;
     private String address;
     private Double rent;
+
+    public Room(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getRent() {
+        return rent;
+    }
+
+    public void setRent(Double rent) {
+        this.rent = rent;
+    }
 }
