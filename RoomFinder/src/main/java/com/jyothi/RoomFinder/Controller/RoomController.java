f12d07347d201006f096e8f00289b337adbabe37
JyothiBhavani1999 <bhavanijyothi182@gmail.com>
2025-11-13 00:34:10 -0600

    update and edit room

--- RoomFinder/src/main/java/com/jyothi/RoomFinder/Controller/RoomController.java
+++ RoomFinder/src/main/java/com/jyothi/RoomFinder/Controller/RoomController.java
@@ -2,7 +2,9 @@ package com.jyothi.RoomFinder.Controller;
 
 import com.jyothi.RoomFinder.Model.Room;
 import com.jyothi.RoomFinder.Repository.RoomRepository;
+import com.jyothi.RoomFinder.Service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,19 +14,13 @@ import java.util.List;
 @Controller
 public class RoomController {
     @Autowired
-    RoomRepository roomRepository;
+    RoomService roomService;
 
     @GetMapping
     public String hello() {
         return "Hello, Room Finder!";
     }
 
-    @GetMapping("/rooms-list")
-    public String listRooms(Model model) {
-        List<Room> rooms = roomRepository.findAll();
-        model.addAttribute("rooms", rooms);
-        return "rooms-list";
-    }
 
     @GetMapping("/add-room")
     public String showForm(){
@@ -33,8 +29,36 @@ public class RoomController {
 
     @PostMapping("/add-room")
     public String addRoom(@ModelAttribute Room room) {
-         roomRepository.save(room);
+         roomService.addRoom(room);
          return "success";
     }
+
+    @GetMapping("/rooms-list")
+    public String listRooms(Model model) {
+        List<Room> rooms = roomService.getAllRooms();
+        model.addAttribute("rooms", rooms);
+        return "rooms-list";
+    }
+
+    @GetMapping("/room/{id}")
+    public String getRoomById(@PathVariable Long id, Model model){
+        Room room = roomService.getRoomById(id);
+        model.addAttribute("room", room);
+        return "room-details";
+    }
+
+    @GetMapping("/edit-room/{id}")
+    public String showEditForm(@PathVariable Long id, Model model){
+       Room room = roomService.getRoomById(id);
+       model.addAttribute("room", room);
+       return "edit-room";
+    }
+
+    @PostMapping("/update-room")
+    public String updateRoom( @ModelAttribute Room room) {
+        roomService.updateRoom( room);
+        return "redirect:/rooms-list";
+    }
+
 
 }
